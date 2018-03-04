d54d015dc9e0687dee4932167e96b79c85a8a205
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2018-03-04 21:58:09 +0530

    use scrollToElement to jump through marked text

--- src/content-page/content.js
+++ src/content-page/content.js
@@ -1,15 +1,60 @@
 import Mark from 'mark.js';
+import scrollToElement from 'scroll-to-element';
 
 const $ = (selector) => document.querySelector(selector);
 const $$ = (selector) => document.querySelectorAll(selector);
 
 const marker = new Mark(document.body);
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	const txt = request.txt;
-	marker.unmark();
-	marker.mark(txt);
+let nodesFound = [];
+let nodesIter;
 
-	const markNodes = Array.from($$('[data-markjs="true"]'));
-	console.log(markNodes);
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	if (request.txt) {
+		const txt = request.txt;
+		marker.unmark();
+		marker.mark(txt);
+
+		nodesFound = Array.from($$('[data-markjs="true"]'));
+		nodesIter = cyclicIterator(nodesFound);
+	}
+	if (request.jump) {
+		let currNode = nodesIter.getCurrent();
+		currNode.style.backgroundColor = '#ff0';
+
+		if (request.jump === 'next') {
+			currNode = nodesIter.getNext();
+		} else if (request.jump === 'previous') {
+			currNode = nodesIter.getPrevious();
+		}
+		scrollToElement(currNode, {
+			offset: 0,
+			ease: 'linear',
+			duration: 100
+		});
+		currNode.style.backgroundColor = '#ff9696';
+	}
 });
+
+function cyclicIterator(array) {
+    var index = 0;
+    var copy = array.slice(0);
+
+    return {
+        getCurrent: function () {
+            return copy[index];
+        },
+
+        getNext: function () {
+            index = ++index % copy.length;
+            return this.getCurrent();
+        },
+
+        getPrevious: function () {
+            if(--index < 0) {
+              index += copy.length;
+            }
+            return this.getCurrent();
+        }
+    };
+};
--- src/popup-page/popup.js
+++ src/popup-page/popup.js
@@ -4,6 +4,19 @@ $('#textInput').addEventListener('input', function(event) {
 	const message = {
 		txt: this.value
 	};
+	sendMessage(message);
+});
+
+
+$('#nextBtn').addEventListener('click', function(event) {
+	sendMessage({ jump: 'next' });
+});
+
+$('#prevBtn').addEventListener('click', function(event) {
+	sendMessage({ jump: 'previous' });
+});
+
+function sendMessage(message) {
 	const params = {
 		active: true,
 		currentWindow: true
@@ -11,4 +24,4 @@ $('#textInput').addEventListener('input', function(event) {
 	chrome.tabs.query(params, (tabs) => {
 		chrome.tabs.sendMessage(tabs[0].id, message);
 	});
-});
+}
