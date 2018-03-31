b774b5c736f0936d4c21ea292825cc4613c224c0
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2018-04-01 00:37:30 +0530

    resize body on result find and show results as links

--- src/popup-page/popup.js
+++ src/popup-page/popup.js
@@ -1,12 +1,17 @@
 $('#search-input').on('input', function(event) {
 	const query = $(this).val();
 	chrome.bookmarks.search(query, results => {
+		if (results.length > 0) {
+			$('body').addClass('res-present');
+		} else {
+			$('body').removeClass('res-present');
+		}
+
 		$('.results-box').empty();
 		const resultNodes = results.map(res => {
 			const resElem = `
 				<div>
-					<h3>${res.title}</h3>
-					<p>${res.url}</p>
+					<h3><a href="${res.url}" target="_blank">${res.title}</a></h3>
 				</div>
 			`;
 			return $(resElem);
