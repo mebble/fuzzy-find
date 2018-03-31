b836e549921668d8d7e7ebdbb66568444e927327
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2018-04-01 00:28:26 +0530

    remove message passing code

--- src/popup-page/popup.js
+++ src/popup-page/popup.js
@@ -14,13 +14,3 @@ $('#search-input').on('input', function(event) {
 		$('.results-box').append(resultNodes);
 	});
 });
-
-// function sendMessage(message) {
-// 	const params = {
-// 		active: true,
-// 		currentWindow: true
-// 	};
-// 	chrome.tabs.query(params, (tabs) => {
-// 		chrome.tabs.sendMessage(tabs[0].id, message);
-// 	});
-// }
